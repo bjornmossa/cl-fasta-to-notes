1446b93357498a35b367bf3594be8b21f37e9db4
lambdaDude <aprosvirnin@textback.io>
2019-01-11 19:50:45 +0300

    add func for output SuperCollider patterns

--- output.lisp
+++ output.lisp
@@ -5,3 +5,7 @@
         #:fasta-notes))
 
 (in-package #:fasta-notes.output)
+
+(defun print-sc-code (amps durs &optional (stream t))
+  (format stream "pdef({~C~C  \\amp, Pseq([~{~a~^, ~}], inf),~C~C  \\dur, Pseq([~{~a~^, ~}], inf)~C~C});"
+          #\return #\linefeed amps #\return #\linefeed durs #\return #\linefeed))
