d562823dac5b193222cb70580eb0a2649518e9f2
Arseniusz <prosvirnin.arseniy@gmail.com>
2020-02-14 09:09:15 +0300

    Remove old functions

--- fasta-notes.lisp
+++ fasta-notes.lisp
@@ -2,76 +2,6 @@
 
 (in-package #:fasta-notes)
 
-(defun get-nuc-assocs ()
-  "Return list of cons pairs where car is nucleotide char and cdr is int"
-  '((#\A . 0) (#\G . 1) (#\T . 2) (#\C . 3)))
-
-(defun get-stop-codons ()
-  "Return list of stop codons as Strings"
-  '("TGA" "TAG" "TAA"))
-
-(defun get-nuc-assoc-val (nuc assoc)
-  "Return Integer if nucleotide is in cons pair or nil"
-  (if (char= nuc (car assoc))
-      (cdr assoc)
-      nil))
-
-(defun get-assoc-numlist (nuc)
-  "Return list of possible associations for nucleotide or nils"
-  (map
-   'list
-   #'(lambda (x) (get-nuc-assoc-val nuc x))
-   (get-nuc-assocs)))
-
-(defun find-nuc-assoc (nuc)
-  "Return Integer, associated with nucleotide or nil"
-  (find-if-not #'null (get-assoc-numlist nuc)))
-
-(defun is-nuc? (n)
-  "Nucleotide predicate"
-  (not (null (find-nuc-assoc n))))
-
-(defun str-to-list (str)
-  "Turn string into list of chars"
-  (coerce str 'list))
-
-(defun nuc-string? (str)
-  (every #'is-nuc? (str-to-list str)))
-
-(defun is-stop-codon? (str)
-  "Stop codon predicate"
-  (member str (get-stop-codons) :test #'string-equal))
-
-(defun codon-to-assocs (str)
-  (map 'list #'find-nuc-assoc (str-to-list str)))
-
-(defun codon-to-notes (str)
-  "Return cons pair represent a note and dur"
-  (let ((assocs (codon-to-assocs str)))
-    (cons
-     (car assocs)
-     (+ (second assocs) (third assocs)))))
-
-(defun split-string-by (string n &optional result)
-  (if (< (length string) n)
-      result
-      (split-string-by
-       (subseq string n)
-       n
-       (append result (list (subseq string 0 n))))))
-
-(defun get-dur (int)
-  (nth int '(1 2 4 8 16 32 64 128)))
-
-(defun create-model (string)
-  (if (is-stop-codon? string)
-      (make-codon-note :is-pause t
-                       :dur (get-dur (car (codon-to-notes string))))
-      (make-codon-note :dur (get-dur (car (codon-to-notes string)))
-                       :degree (cdr (codon-to-notes string)))))
-
-
-;; NEW STUFF
 (defun nucleotide-list ()
   '(A G T C))
 
@@ -92,6 +22,12 @@
    (reduce #'+ (butlast codon))
    (reduce #'+ (rest codon))))
 
+(defun codon-value (codon)
+  (let ((val (get-codon-values codon)))
+      (cons
+       (reduce #'+ (butlast val))
+       (reduce #'+ (rest val)))))
+
 (defun get-dur (int)
   (nth int '(1 1/2 1/4 1/8 1/16 1/32 1/64 1/128)))
 
@@ -110,9 +46,9 @@
                    '((T G A) (T A G) (T A A))))))
 
 (defun create-codon-model (codon)
-  (let ((value (codon-value codon)))
-    (if (stop-codon-p value)
+  (let ((val (codon-value codon)))
+    (if (stop-codon-p val)
         (make-codon-note :is-pause t
-                         :dur (get-dur (cdr value)))
-        (make-codon-note :degree (car value)
-                         :dur (get-dur (cdr value))))))
+                         :dur (get-dur (cdr val)))
+        (make-codon-note :degree (car val)
+                         :dur (get-dur (cdr val))))))
