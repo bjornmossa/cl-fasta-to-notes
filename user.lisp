47aab4681798c7cbb52598aa6e7cacd9f0f9b975
Arseniy <arseniy.prosvirnin@mediaopt.de>
2020-05-01 10:10:17 +0300

    Preparations for refactoring files saving

--- user.lisp
+++ user.lisp
@@ -4,12 +4,11 @@
   (:use #:cl
         #:fasta-notes
         #:fasta-notes.input
+        #:fasta-notes.output
         #:fasta-notes.model))
 
 (in-package :fasta-notes.user)
 
-;; load file
-;; show file info
 (defparameter *file* '(ERROR . "No file selected"))
 
 (defmacro with-file-env (file-env &rest body)
@@ -18,6 +17,21 @@
       (if (eq (car ,file) 'ERROR)
           (cdr ,file)
           ,@body))))
+
+(defmacro with-path (var &body body)
+  (let ((path (gensym)))
+    `(progn
+      (print "Enter path to save a file:")
+      (let ((,path (read-line)))
+        (if (null (directory ,path))
+            (print "Directory doesn't exist")
+            (let ((,var ,path))
+              ,@body))))))
+
+(defun get-safe-name (file-header-content)
+  (let ((first-space (+ 1 (position #\Space file-header-content)))
+        (header-length (length file-header-content)))
+    (subseq file-header-content first-space header-length)))
 
 (defun load-file ()
   (print "Enter path to .fasta file:")
@@ -41,3 +55,23 @@
            (durs (map 'list #'codon-note-dur sequence))
            (degrees (map 'list #'codon-note-degree sequence)))      
       (format t "Degrees: ~{~a~^, ~}~%Durations: ~{~a~^, ~}" degrees durs))))
+
+(defun save-sc-file (start end)
+  (with-path p
+    (let* ((path (concatenate 'string p "/" (get-safe-name (fasta-file-header (cdr *file*))) ".scd"))
+           (codons (fasta-file-content (cdr *file*))))
+      (print "ok"))))
+
+
+(defun save-sequence ()
+  (with-file-env *file*
+    (progn
+      (format t "~%Choose format.~%1 - SuperCollider~%2 - LilyPond~%")
+      (let ((choose (read-preserving-whitespace)))
+      (cond
+        ((eql choose 1)
+         (format t "SC"))
+        ((eql choose 2)
+         (format t "LY"))
+        (t
+         (format t "Wrong option chosen")))))))
