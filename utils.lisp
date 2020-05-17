8193ec213dad3cf16e156a0ffc1895efd0b9e6d1
Arseniy <arseniy.prosvirnin@mediaopt.de>
2020-05-17 22:52:00 +0300

    Add function for getting short name or organism in fasta file

--- utils.lisp
+++ utils.lisp
@@ -1,8 +1,10 @@
 (in-package :cl-user)
 
 (defpackage #:fasta-notes.utils
-  (:use #:cl)
-  (:export #:zip-list))
+  (:use #:cl
+        #:cl-ppcre)
+  (:export #:zip-list
+           #:safe-organism-name))
 
 (in-package #:fasta-notes.utils)
 
@@ -13,3 +15,12 @@
        (subseq lst len (length lst))
        len
        (cons (subseq lst 0 len) result))))
+
+(defun words-only (string)
+  (format nil "~{~A~^~}" (all-matches-as-strings "\\w+" string)))
+
+(defun get-organism-names (fasta-header)
+  (subseq (cdr (split "\\s+" fasta-header)) 0 2))
+
+(defun safe-organism-name (fasta-header)
+  (format nil "~{~(~a~)~^_~}" (map 'list #'words-only (get-organism-names fasta-header))))
